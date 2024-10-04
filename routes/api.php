1aca165f5e27762105459eddc7e97e261915797c
Amador Quispe <aquispe.dev@gmail.com>
2024-10-03 22:31:12 -0500

    se agrega las rutas del api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/products', [ProductController::class, 'getAll']);
+
+Route::get('/products/{id}', [ProductController::class, 'getById']);
+
+Route::put('/products/{id}', [ProductController::class, 'update']);
+
+Route::post('/products', [ProductController::class, 'create']);
+
+Route::delete('/products/{id}', [ProductController::class, 'delete']);
